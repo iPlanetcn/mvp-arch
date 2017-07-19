352cc34c8e304821b5e7ca11c74f7b604d86f0f3
John <iplanetcn@gmail.com>
2017-07-19 11:20:46 +0800

    update the config files

--- app/src/androidTest/java/com/childcare/app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/childcare/app/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.o6care.growthcare", appContext.getPackageName());
+        assertEquals("com.childcare.app", appContext.getPackageName());
     }
 }
